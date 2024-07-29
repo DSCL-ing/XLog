4712415b6ac3be2873fdc9030ed2ab017fd68a23
chj <2639075023@qq.com>
2024-07-29 15:54:04 +0800

    update

--- code_linux/logs/buffer.hpp
+++ code_linux/logs/buffer.hpp
@@ -1,6 +1,9 @@
 #ifndef BUFFER_HPP
 #define BUFFER_HPP
 
+#include<iostream>
+#include<vector>
+#include<cassert>
 
 
 //同步写日志过程(直接落地)可能写的比较慢,写入量多等,为了避免因写日志过程阻塞带来的影响,实现异步落地日志器
@@ -22,7 +25,7 @@
   */
 
  /*
-  缓冲区设计: 一个缓冲区负责输入,另一个缓冲区负责输出; 当其中一个满足条件时,进行角色交换;另一个为空时,不交换
+  缓冲区设计: 一个缓冲区负责输入,另一个缓冲区负责输出; 当输出完毕,初始化缓冲区,然后角色交换(输入缓冲区也为空时,不交换);
   存储数据类型: 只存储格式化好的字符串 -- 避免Msg对象频繁创建带来的性能开销
   数据结构: std::vector -- 
   读指针rseek: 指向当前读取的位置,读到写指针位置时,表明读完,交换
@@ -30,17 +33,19 @@
 
   提供的缓冲区管理操作:
   - 读写操作
-    1.向缓冲区中写入数据 push()
+    1.向缓冲区中写入数据 push() 
+        .写指针偏移moveWrite    不需要写地址,push内部实现   .可写数据长度 writeAbleSize()
     2.从缓冲区中读入数据 (虚:读数据会再次发生拷贝,原位置不需要保留 --> 不读,直接移动 --> 几乎与同步日志效率一样)
-    -.读写指针偏移  1.读指针偏移moveReader 2.写指针偏移moveWrite 3.获取可读数据起始地址begin 4.获取可读数据长度readAbleSize() 5.可写数据长度
+        .读指针偏移moveReader   .获取可读数据起始地址begin  .获取可读数据长度readAbleSize() 
   - 初始化 reset()
-  - 交换(地址交换) 1.empty()
+  - 交换(地址交换) 
+  - empty()
  
 logic:
     buffer( rseek, wseek, _vector<字节流>)
-        写入push --- len>writeAbleSize()? 扩容,写入:写入 --- moveWseek()
-            扩容ensure
-        读出 Read: ReadAbleSize + moveRseek
+        写入push --- len>writeAbleSize()? 扩容,写入:写入 --- moveWseek(单位)
+            扩容ensureEnouthSize
+        读出 --->  stream.write() : ReadAbleSize + moveRseek
             
 
 
@@ -48,19 +53,86 @@ logic:
  */
 
 namespace log{
+  #define DEFAULT_BUFFER_SIZE 1*1024*1024  //1M
+  #define THRESHOLD_BUFFER_SIZE 5*1024*1024 //5M -- 阈值以下,翻倍增长;阈值以上 线性增长
+  #define INCREMENT_BUFFER_SIZE 1*1024*1024 //1M -- 线性增长,增量大小
+                                            
+                                           
     class Buffer{
     public:
         Buffer()
+        :_buffer(DEFAULT_BUFFER_SIZE),_rindex(0),_windex(0)
         {} 
-        void push(){
-            
-        }
-        
-        //返回可读数据的起始地址
-        const char* begin(){
-            
+
+        void push(const char* data,size_t len){
+          //空间不足: 1.阻塞(实际业务,空间有限),返回false  2.扩容(性能测试)
+          
+          // if(len>writeAbleSize()) return; //定容
+          
+          if(len>=writeAbleSize()){
+            ensureEnoughSize(len); //扩容
+          }
+          std::copy(data,data+len,&_buffer[_windex]);
+          moveWrite(len);
         }
 
+        //返回可写大小
+        size_t writeAbleSize() { return _buffer.size() - _windex; }
+
+        //返回可读大小
+        size_t readAbleSize(){ return _windex - _rindex; }
+          // IF: _widx == _ridx ==0 ; ---> push 1; _widx+=1;  THEN: _widx-_ridx==1-0=1 == readAbleSize;
+       
+        //移动读指针
+        void moveReader(size_t len){
+          assert(len<=readAbleSize()); 
+          _rindex += len;
+        }
+
+        //移动写指针
+        void moveWrite(size_t len) {
+          assert(len<=writeAbleSize()); //针对定容情况
+          _windex += len;
+        }
+
+        //返回可读数据的起始地址
+        const char* begin(){ return &_buffer[_rindex]; }
+
+
+        void swap(Buffer &buffer){
+          _buffer.swap(buffer._buffer);
+          std::swap(buffer._rindex,_rindex);
+          std::swap(buffer._windex,_windex);
+        }
+
+        //判空
+        bool empty() { return _windex - _rindex == 0 ? true : false; } // 相等为空
+        
+        //初始化 : 交换前置空
+        void reset(){
+          _rindex = _windex = 0;
+        }
+
+      private:
+        //扩容 -- 确保有足够空间
+        void ensureEnoughSize(size_t len){ //简单复现 -- linux 网络IO 拥塞控制
+          if (len < writeAbleSize()) return;
+          size_t new_capacity = 0;
+          if (_buffer.size() < THRESHOLD_BUFFER_SIZE) {
+            new_capacity = _buffer.size() * 2 + len;
+          }
+          else {
+            new_capacity = _buffer.size() + INCREMENT_BUFFER_SIZE + len;
+          }
+          _buffer.resize(new_capacity);
+          //+len确保扩容的大小足以容纳len; 或者不加len,使用循环扩容直到容纳len;
+        }
+        
+
+      private:
+       std::vector<char> _buffer; //字节流vector -- 
+       size_t _rindex;
+       size_t _windex; 
     };
 
 }//namespace_log__END
--- code_linux/logs/test.cc
+++ code_linux/logs/test.cc
@@ -1,112 +1,176 @@
 
-#include"level.hpp"
-#include"util.hpp"
-#include"message.hpp"
-#include"format.hpp"
-#include"sink.hpp"
-#include"my_sink.h"
-#include"logger.hpp"
+#include "level.hpp"
+#include "util.hpp"
+#include "message.hpp"
+#include "format.hpp"
+#include "sink.hpp"
+#include "my_sink.h"
+#include "logger.hpp"
+#include "buffer.hpp"
 
-void Test_Util(){
+void Test_Util()
+{
   time_t t = log::util::DateUtil::getCurTime();
-  std::cout<<ctime(&t)<<"\n";
+  std::cout << ctime(&t) << "\n";
   std::string pathname = "abc/def/g/test.txt";
   log::util::FileUtil::createDirectory(log::util::FileUtil::getPath(pathname));
 }
 
-void Test_LogLevel(){
-  std::cout<<log::LogLevel::toString(log::LogLevel::Value::DEBUG)<<"\n";
-  std::cout<<log::LogLevel::toString(log::LogLevel::Value::INFO)<<"\n";
-  std::cout<<log::LogLevel::toString(log::LogLevel::Value::WARN)<<"\n";
-  std::cout<<log::LogLevel::toString(log::LogLevel::Value::ERROR)<<"\n";
-  std::cout<<log::LogLevel::toString(log::LogLevel::Value::FATAL)<<"\n";
-  std::cout<<log::LogLevel::toString(log::LogLevel::Value::OFF)<<"\n";
-  std::cout<<log::LogLevel::toString(log::LogLevel::Value::UNKNOW)<<"\n";
+void Test_LogLevel()
+{
+  std::cout << log::LogLevel::toString(log::LogLevel::Value::DEBUG) << "\n";
+  std::cout << log::LogLevel::toString(log::LogLevel::Value::INFO) << "\n";
+  std::cout << log::LogLevel::toString(log::LogLevel::Value::WARN) << "\n";
+  std::cout << log::LogLevel::toString(log::LogLevel::Value::ERROR) << "\n";
+  std::cout << log::LogLevel::toString(log::LogLevel::Value::FATAL) << "\n";
+  std::cout << log::LogLevel::toString(log::LogLevel::Value::OFF) << "\n";
+  std::cout << log::LogLevel::toString(log::LogLevel::Value::UNKNOW) << "\n";
 }
 
-void Test_Formatter(){
-  log::LogMsg msg(log::LogLevel::Value::DEBUG,"test.cc",12,"root","创建文件失败");
-  log::Formatter fmt("abc[%d{%H:%M:%S}]%%[%t][%p][%c][%f:%l]%T %m%n"); 
-  std::cout<<fmt.format(msg);
+void Test_Formatter()
+{
+  log::LogMsg msg(log::LogLevel::Value::DEBUG, "test.cc", 12, "root", "创建文件失败");
+  log::Formatter fmt("abc[%d{%H:%M:%S}]%%[%t][%p][%c][%f:%l]%T %m%n");
+  std::cout << fmt.format(msg);
 }
 
-void  Test_LogSink(){
-  //std::shared_ptr<log::LogSink> lsp = log::SinkFactory::create<log::StdoutSink>();
-  //auto lsp = log::SinkFactory::create<log::FileSink>("logs/log");
-  //auto lsp = log::SinkFactory::create<log::RollBySizeSink>("logs/roll-",1024*1024);
-  std::shared_ptr<log::LogSink> lsp = log::SinkFactory::create<RollbyTimeSink>("logs/roll-",TimeGap::SECOND);
+void Test_LogSink()
+{
+  // std::shared_ptr<log::LogSink> lsp = log::SinkFactory::create<log::StdoutSink>();
+  // auto lsp = log::SinkFactory::create<log::FileSink>("logs/log");
+  // auto lsp = log::SinkFactory::create<log::RollBySizeSink>("logs/roll-",1024*1024);
+  std::shared_ptr<log::LogSink> lsp = log::SinkFactory::create<RollbyTimeSink>("logs/roll-", TimeGap::SECOND);
 
   log::Formatter fmt;
   int count = 0;
 
-  //size_Test
-  //for(int i  = 0;i<1024*1024*100;){
-  //log::LogMsg msg(log::LogLevel::Value::DEBUG,"test.cc",12,"root","创建文件失败");
-  //std::string str = fmt.format(msg);
-  //  std::string tmp = str+std::to_string(count++);
-  //  lsp->log(tmp.c_str(),tmp.size());
-  //  i+=str.size();
-  //}
-  
-  //time_Test
+  // size_Test
+  // for(int i  = 0;i<1024*1024*100;){
+  // log::LogMsg msg(log::LogLevel::Value::DEBUG,"test.cc",12,"root","创建文件失败");
+  // std::string str = fmt.format(msg);
+  //   std::string tmp = str+std::to_string(count++);
+  //   lsp->log(tmp.c_str(),tmp.size());
+  //   i+=str.size();
+  // }
+
+  // time_Test
   time_t start_time = log::util::DateUtil::getCurTime();
-  while(log::util::DateUtil::getCurTime()<start_time+5){
-    log::LogMsg msg(log::LogLevel::Value::DEBUG,"test.cc",12,"root","创建文件失败");
+  while (log::util::DateUtil::getCurTime() < start_time + 5)
+  {
+    log::LogMsg msg(log::LogLevel::Value::DEBUG, "test.cc", 12, "root", "创建文件失败");
     std::string str = fmt.format(msg);
-    std::string tmp = str+std::to_string(count++);
-    lsp->log(tmp.c_str(),tmp.size());
-
+    std::string tmp = str + std::to_string(count++);
+    lsp->log(tmp.c_str(), tmp.size());
   }
-
 }
 
-void Test_Logger(){
-  //直接零部件构造太麻烦 --> 建造者模式,简化使用复杂度
+void Test_Logger()
+{
+  // 直接零部件构造太麻烦 --> 建造者模式,简化使用复杂度
   std::shared_ptr<log::LogSink> lsp1 = log::SinkFactory::create<log::StdoutSink>();
   auto lsp2 = log::SinkFactory::create<log::FileSink>("logsByFile/test.log");
-  auto lsp3 = log::SinkFactory::create<log::RollBySizeSink>("logsBySize/roll-",1024*1024);
-  std::shared_ptr<log::LogSink> lsp4 = log::SinkFactory::create<RollbyTimeSink>("logsByTime/roll-",TimeGap::SECOND);
-  std::vector<std::shared_ptr<log::LogSink>> sinks{lsp1,lsp2,lsp3,lsp4};
-  log::SyncLogger sl("root",log::LogLevel::Value::INFO,std::make_shared<log::Formatter>("[%d{%H:%M:%S}][%t][%p][%c][%f:%l] %m%n"),sinks);
+  auto lsp3 = log::SinkFactory::create<log::RollBySizeSink>("logsBySize/roll-", 1024 * 1024);
+  std::shared_ptr<log::LogSink> lsp4 = log::SinkFactory::create<RollbyTimeSink>("logsByTime/roll-", TimeGap::SECOND);
+  std::vector<std::shared_ptr<log::LogSink>> sinks{lsp1, lsp2, lsp3, lsp4};
+  log::SyncLogger sl("root", log::LogLevel::Value::INFO, std::make_shared<log::Formatter>("[%d{%H:%M:%S}][%t][%p][%c][%f:%l] %m%n"), sinks);
 
   time_t start_time = log::util::DateUtil::getCurTime();
   size_t count = 0;
-  while(log::util::DateUtil::getCurTime()<start_time+5){
-    sl.debug(__FILE__,__LINE__,"%s-%d","打开文件失败",count++);
-    sl.info(__FILE__,__LINE__,"%s-%d","打开文件失败",count);
-    sl.warn(__FILE__,__LINE__,"%s-%d","打开文件失败",count);
-    sl.error(__FILE__,__LINE__,"%s-%d","打开文件失败",count);
-    sl.fatal(__FILE__,__LINE__,"%s-%d","打开文件失败",count);
+  while (log::util::DateUtil::getCurTime() < start_time + 5)
+  {
+    sl.debug(__FILE__, __LINE__, "%s-%d", "打开文件失败", count++);
+    sl.info(__FILE__, __LINE__, "%s-%d", "打开文件失败", count);
+    sl.warn(__FILE__, __LINE__, "%s-%d", "打开文件失败", count);
+    sl.error(__FILE__, __LINE__, "%s-%d", "打开文件失败", count);
+    sl.fatal(__FILE__, __LINE__, "%s-%d", "打开文件失败", count);
   }
 }
 
-void Test_Builder(){
-  std::unique_ptr<log::LoggerBuilder> builder (new log::LocalLoggerBuilder());
+void Test_Builder()
+{
+  std::unique_ptr<log::LoggerBuilder> builder(new log::LocalLoggerBuilder());
   builder->buildFormatter();
   // builder->buildLoggerLevel();
   builder->buildLoggerName("root");
-  builder->buildSink<RollbyTimeSink>("logsByTime/roll-",TimeGap::SECOND);
-  builder->buildSink<log::RollBySizeSink>("logsBySzie/roll-",1024*1024);
-  builder->buildSink<log::FileSink>("logsByfile");
+  // builder->buildSink<RollbyTimeSink>("logsByTime/roll-", TimeGap::SECOND);
+  // builder->buildSink<log::RollBySizeSink>("logsBySzie/roll-", 1024 * 1024);
+  builder->buildSink<log::FileSink>("logsByfile/file.log");
   // builder->buildLoggerType();
-  //builder->buildSink();
-  auto  sl= builder->build();
+  // builder->buildSink();
+  auto sl = builder->build();
   time_t start_time = log::util::DateUtil::getCurTime();
   size_t count = 0;
-  while(log::util::DateUtil::getCurTime()<start_time+2){
-    sl->debug(__FILE__,__LINE__,"%s-%d","打开文件失败",count++);
-    sl->info(__FILE__,__LINE__,"%s-%d","打开文件失败",count);
-    sl->warn(__FILE__,__LINE__,"%s-%d","打开文件失败",count);
-    sl->error(__FILE__,__LINE__,"%s-%d","打开文件失败",count);
-    sl->fatal(__FILE__,__LINE__,"%s-%d","打开文件失败",count);
-}
+  while (log::util::DateUtil::getCurTime() < start_time + 2) {
+    sl->debug(__FILE__, __LINE__, "%s-%d", "打开文件失败", count++);
+    sl->info(__FILE__, __LINE__, "%s-%d", "打开文件失败", count);
+    sl->warn(__FILE__, __LINE__, "%s-%d", "打开文件失败", count);
+    sl->error(__FILE__, __LINE__, "%s-%d", "打开文件失败", count);
+    sl->fatal(__FILE__, __LINE__, "%s-%d", "打开文件失败", count);
+  }
 }
 
-int main(){
-  //Test_LogLevel();
-  //Test_Formatter();
-  //Test_LogSink();
-  //Test_Logger();
-  Test_Builder();
+void Test_Buffer(){
+  //测试思路:
+  /*
+  提前准备好本地文件
+    将本地文件读入内存 
+    将内存中数据写入到buffer
+    将buffer中数据输出到文件  
+  对比文件是否相同 
+  */
+
+  std::ifstream ifs("logsByfile/file.log",std::ios::binary);
+  if(ifs.is_open() == false){
+    std::cout<<"ifs open fail\n";
+    abort();
+  }
+  ifs.seekg(0,std::ios::end); //文件指针跳转到相对于文件末尾+0的位置
+  size_t size = ifs.tellg() ; //获取文件指针当前的位置
+  ifs.seekg(std::ios::beg); //文件指针跳转到绝对位置beg,即文件起始
+  
+  //读到内存
+  std::string body;
+  body.resize(size);
+  ifs.read(&body[0],size); //read(char* ,len); //字节流
+  if(ifs.good() == false){
+    std::cout<<"ifs good fail\n";
+    abort();
+  }
+  ifs.close();
+
+  
+  //写到缓冲区
+  log::Buffer buffer;
+  for(size_t i = 0; i<body.size();i++){
+    buffer.push(&body[i],1);
+  }
+  
+  //缓冲区写到文件 -- 尽可能测试更多的接口
+  std::ofstream ofs("./logsByfile/out.log",std::ios::binary);
+  if(ofs.is_open() == false){
+    std::cout<<"ifs is_open fail\n";
+    abort();
+  }
+
+  size_t buffersize = buffer.readAbleSize();
+
+  // for(size_t i = 0; i<buffersize;i++){
+  //   ofs.write(buffer.begin(),1);
+  //   buffer.moveReader(1);
+  // }
+  ofs.write(buffer.begin(),buffersize);
+  
+
+  //util : md5sum 比较
+}
+
+int main()
+{
+  // Test_LogLevel();
+  // Test_Formatter();
+  // Test_LogSink();
+  // Test_Logger();
+  // Test_Builder();
+  Test_Buffer();
   return 0;
 }
